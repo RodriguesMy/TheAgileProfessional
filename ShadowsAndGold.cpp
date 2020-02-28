f9507b5c55cc7ba2ef933fad8ccb12feba747868
Myriam <mrodrigues3@uclan.ac.uk>
2020-02-28 10:51:53 +0200

    Added camera and model movement

--- ShadowsAndGold.cpp
+++ ShadowsAndGold.cpp
@@ -3,9 +3,46 @@
 #include <TL-Engine.h>	// TL-Engine include file and namespace
 using namespace tle;
 #include <iostream>
-#include <time.h>
 
-#include "CThief.h"
+void UpdateModel(I3DEngine* myEngine,IModel* pThief,float thiefMovementSpeed,float &dt)
+{
+	if (myEngine->KeyHeld(Key_W)) {
+		pThief->MoveLocalZ(thiefMovementSpeed*dt);
+	}
+	if (myEngine->KeyHeld(Key_S)) {
+		pThief->MoveLocalZ(-thiefMovementSpeed * dt);
+	}
+	if (myEngine->KeyHeld(Key_D)) {
+		pThief->MoveLocalX(thiefMovementSpeed*dt);
+	}
+	if (myEngine->KeyHeld(Key_A)) {
+		pThief->MoveLocalX(-thiefMovementSpeed * dt);
+	}
+}
+void UpdateCamera(I3DEngine* myEngine,IModel* pThief,float &cameraAngle,float maxCameraRotation,IModel* pCameraDummy,float minCameraRotation)
+{
+	float cameraMovementY = myEngine->GetMouseMovementY();
+	float cameraMovementX = myEngine->GetMouseMovementX();
+
+	pThief->RotateY(cameraMovementX);
+	//pCameraDummy->RotateY(cameraMovementX);
+
+	if (cameraMovementY > 0)
+	{
+		if (cameraAngle < maxCameraRotation)
+		{
+
+			pCameraDummy->RotateLocalX(cameraMovementY);
+			cameraAngle += cameraMovementY;
+		}
+	}
+	else if (cameraMovementY < 0) {
+		if (cameraAngle > minCameraRotation) {
+			pCameraDummy->RotateLocalX(cameraMovementY);
+			cameraAngle += cameraMovementY;
+		}
+	}
+}
 void main()
 {
 	// Create a 3D engine (using TLX engine here) and open a window for it
@@ -16,7 +53,13 @@ void main()
 	myEngine->AddMediaFolder( "./Media" );
 
 	/**** Set up your scene here ****/
-	ICamera* camera = myEngine->CreateCamera(kManual, 0, 15, -25);
+	IMesh* pFloorMesh = myEngine->LoadMesh("Floor.x");
+	IModel* pFloor = pFloorMesh->CreateModel(0,-0.3,0);
+
+	IMesh* pDummyMesh = myEngine->LoadMesh("dummy.x");
+	IModel* pCameraDummy = pDummyMesh->CreateModel();
+
+	ICamera* camera = myEngine->CreateCamera();
 	camera->RotateX(25);
 
 	//IMPORTANT VARIABLES
@@ -24,29 +67,33 @@ void main()
 	float thiefMovementSpeed = 10;
 
 	//Create Thief
-	CThief* Thief = new CThief(myEngine);
+	IMesh* pThieflMesh = myEngine->LoadMesh("sierra.x");
+	IModel* pThief = pThieflMesh->CreateModel();
+	
+	//Rotation of camera variables
+	float maxCameraRotation = 40;
+	float cameraAngle = 25;
+	float minCameraRotation = 0;
+
+	//END OF IMPORTANT VARIABLES 
+
+	//Setting Position & Attaching models
+	camera->SetPosition(pThief->GetX(), pThief->GetY() + 3, pThief->GetZ() - 3);
+
+	camera->AttachToParent(pCameraDummy);
+	pCameraDummy->AttachToParent(pThief);
 
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
 	{
 		// Draw the scene
 		myEngine->DrawScene();
+		dt = myEngine->Timer();
 
 		/**** Update your scene each frame here ****/
-		//thiefControls
-		dt = myEngine->Timer();
-		/*if (myEngine->KeyHeld(Key_W)) {
-			thief->MoveLocalZ(thiefMovementSpeed*dt);
-		}
-		if (myEngine->KeyHeld(Key_S)) {
-			thief->MoveLocalZ(-thiefMovementSpeed * dt);
-		}
-		if (myEngine->KeyHeld(Key_D)) {
-			thief->MoveLocalX(thiefMovementSpeed*dt);
-		}
-		if (myEngine->KeyHeld(Key_A)) {
-			thief->MoveLocalX(-thiefMovementSpeed * dt);
-		}*/
+		UpdateModel(myEngine, pThief, thiefMovementSpeed, dt);
+		UpdateCamera(myEngine, pThief, cameraAngle, maxCameraRotation, pCameraDummy, minCameraRotation);
+		
 	}
 
 	// Delete the 3D engine now we are finished with it
